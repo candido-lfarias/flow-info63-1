369b82cc800f6ded29308b3a51cfec2468159426
Mortadela011 <pietrohoffmann0@gmail.com>
2025-03-25 10:27:41 -0300

    Adiciona sistema de login

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { promises } = require('dns');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -12,6 +13,13 @@ app.get('/api/version', (req, res) => {
 app.get('/api/candido-lfarias', (req, res) => {
 	
   res.json({ version: '1.0.0' });
+});
+
+app.get('/api/pietro/12', async (req, res) => {
+	const fs = require('fs'). promises;
+  const data= await fs.readFile('pietro.txt', 'utf8');
+  console.log(data);
+  res.json(data);
 });
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
