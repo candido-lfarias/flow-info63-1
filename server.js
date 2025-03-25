a386a5ae44b766ba3992af4c31f656576a7fb5b0
amandaplentz <amanda-5599898@educar.rs.gov.br>
2025-03-25 10:39:22 -0300

    nova funcionalidade

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/api/candido-lfarias', (req, res) => {
 	
   res.json({ version: '1.0.0' });
 });
+
+app.get('/api/amanda', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile("amanda.txt", "utf8");
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
