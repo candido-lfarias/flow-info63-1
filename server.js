b43484776ee7af43c607a3a2501ae2230aab5063
juuliaaj <juliajardim765@gmail.com>
2025-03-25 10:38:42 -0300

    Atualização API

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/api/candido-lfarias', (req, res) => {
 	
   res.json({ version: '1.0.0' });
 });
+
+app.get('/api/julia', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile("julia-6575875.txt", "utf8");
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
