3c63134abd11010a78ed8b1b98c369beaf573f8a
nicoli-mb <nicolibenk@gmail.com>
2025-03-25 10:38:39 -0300

    atualização API

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/api/candido-lfarias', (req, res) => {
 	
   res.json({ version: '1.0.0' });
 });
+app.get('/api/nicoli', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile("nicoli-6575878.txt", "utf8");
+  res.json(data);
+});
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
