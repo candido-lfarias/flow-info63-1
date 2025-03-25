5b4296de09308db5aca9753573d2a93f6732ffd3
thais <thais-pacheco@educar.rs.gov.br>
2025-03-25 10:38:59 -0300

    atualização api

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/api/candido-lfarias', (req, res) => {
 	
   res.json({ version: '1.0.0' });
 });
+
+app.get('/api/thais', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile("thais-pacheco.txt", "utf8");
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
